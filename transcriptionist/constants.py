fbbdabf2cadc251be553ef24f69ffe346b79f6e9
Florian <florian@florian.me.uk>
2021-06-15 13:09:13 +0200

    Reduced complexity of Direction.symbol()

--- transcriptionist/constants.py
+++ transcriptionist/constants.py
@@ -14,7 +14,8 @@ class EditOperation(Flag):
         INSERT: Indicates that an insert operation has taken place.
         DELETE: Indicates that a delete operation has taken place.
         SUBSTITUTE: Indicates that a substitute operation has taken place.
-        TRANSPOSE: Indicates that a transpose operation has taken place."""
+        TRANSPOSE: Indicates that a transpose operation has taken place.
+    """
 
     NONE = 0
     INSERT = auto()
@@ -23,8 +24,11 @@ class EditOperation(Flag):
     TRANSPOSE = auto()
 
     def code(self) -> str:
-        """Returns a string representation of the edit operation assigning a single
-        character to each operation."""
+        """Returns a single character string representation of the flag.
+
+        Returns:
+            A single character string representation of the flag.
+        """
         buf = ""
         for name, member in EditOperation.__members__.items():
             if self & member:
@@ -79,27 +83,28 @@ class Direction(Flag):
     NORTHWEST = NORTH | WEST
 
     def symbol(self) -> str:
-        """Returns a symbolic representation in the form of a unicode pointing arrow
-        glyph or small hollow bullet glyph in the case no direction is indicated.
+        """Returns a symbolic representation of the flag.
 
-        Note that glyph representations are only provided for the predefined flags,
-        and not for other potential compound directions such as
-        `Direction.NORTH | Direction.SOUTH`."""
-        if self == Direction.NORTHEAST:
-            return "↗"
-        if self == Direction.SOUTHEAST:
-            return "↘"
-        if self == Direction.SOUTHWEST:
-            return "↙"
-        if self == Direction.NORTHWEST:
-            return "↖"
-        if self == Direction.NORTH:
-            return "↑"
-        if self == Direction.EAST:
-            return "→"
-        if self == Direction.SOUTH:
-            return "↓"
-        if self == Direction.WEST:
-            return "←"
-        if self == Direction.NONE:
-            return "○"
+        Returns a unicode pointing arrow glyph pointing in the direction indicated by
+        the flag, or a small hollow bullet glyph in the case no direction is indicated.
+
+        Note:
+            Glyph representations are only provided for the predefined flags,
+            and not for other potential compound directions such as
+            `Direction.NORTH | Direction.SOUTH`.
+        """
+        mapping = {
+            Direction.NORTHEAST: "↗",
+            Direction.SOUTHEAST: "↘",
+            Direction.SOUTHWEST: "↙",
+            Direction.NORTHWEST: "↖",
+            Direction.NORTH: "↑",
+            Direction.EAST: "→",
+            Direction.SOUTH: "↓",
+            Direction.WEST: "←",
+            Direction.NONE: "○",
+        }
+        if self in mapping:
+            return mapping[self]
+        else:
+            return None
