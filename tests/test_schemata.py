e825a3db6492582ab62b3f4937140b5a418ee55d
Florian <florian@florian.me.uk>
2021-06-18 21:30:11 +0200

    Implemented TargetSegment class and tests.

--- tests/test_schemata.py
+++ tests/test_schemata.py
@@ -1,19 +1,147 @@
 """Unit tests for the `schemata` submodule of `transcriptionist`."""
 import unittest
-from transcriptionist.schemata import Schema
+from transcriptionist.schemata import TargetSegment, TargetSchema
 
 
-class TestSchema(unittest.TestCase):
-    """Unit tests for the `transcriptionist.schemata.Schema` class."""
+class TestTargetSegment(unittest.TestCase):
+    """Unit tests for the `transcriptionist.schemata.TargetSegment` class."""
+
+    def test_segment_creation(self):
+        """Tests initialisation of target segments with various values."""
+        seg = TargetSegment()
+        self.assertEqual(seg.target, None)
+        self.assertEqual(seg.score, 0)
+        seg = TargetSegment("a", 7)
+        self.assertEqual(seg.target, "a")
+        self.assertEqual(seg.score, 7)
+        seg = TargetSegment([1, 2, 3], 6)
+        self.assertEqual(seg.target, [1, 2, 3])
+        self.assertEqual(seg.score, 6)
+
+    def test_segment_from_tuple(self):
+        """Tests that segments can be initialised from a 2-tuple."""
+        seg = TargetSegment.from_pair(("a", 4))
+        self.assertEqual(seg.target, "a")
+        self.assertEqual(seg.score, 4)
+
+    def test_segment_from_list(self):
+        """Tests that segments can be initialised from a 2-member list."""
+        seg = TargetSegment.from_pair(["b", 3])
+        self.assertEqual(seg.target, "b")
+        self.assertEqual(seg.score, 3)
+
+    def test_set_segment_target(self):
+        """Tests reassigning a segment's target value after initialisation."""
+        seg = TargetSegment()
+        seg.target = "d"
+        self.assertEqual(seg.target, "d")
+        seg.target = (1, 2, 3)
+        self.assertEqual(seg.target, (1, 2, 3))
+
+    def test_set_segment_score(self):
+        """Tests reassigning a segment's score value after initialisation."""
+        seg = TargetSegment()
+        seg.score = 16
+        self.assertEqual(seg.score, 16)
+        seg.score = 3.14
+        self.assertEqual(seg.score, 3.14)
+
+    def test_set_invalid_score(self):
+        """Tests that only numeric values can be assigned as score."""
+        # On initialisation
+        with self.assertRaises(TypeError):
+            seg = TargetSegment(score="hi")
+        # Post-initialisation
+        seg = TargetSegment()
+        with self.assertRaises(TypeError):
+            seg.score = "string"
+        with self.assertRaises(TypeError):
+            seg.score = ("t", "u", "p", "l", "e")
+
+    def test_repr(self):
+        """Tests that TargetSegment can be reconstructed from it's repr()."""
+        seg1 = TargetSegment("foo", 42)
+        out = repr(seg1)
+        seg2 = eval(out)  # noqa: S307
+        self.assertEqual(seg1.target, seg2.target)
+        self.assertEqual(seg1.score, seg2.score)
+
+    def test_to_string(self):
+        """Tests that str() returns valid string on TargetSegment."""
+        seg = TargetSegment("foo", 42)
+        out = str(seg)
+        self.assertIsInstance(out, str)
+        self.assertEqual(out, "(foo, 42)")
+
+    def test_hashes(self):
+        """Tests that hash() values are equal to (target, score) tuples."""
+        seg = TargetSegment("foo", 42)
+        self.assertEqual(hash(seg), hash(("foo", 42)))
+        seg.score += 1
+        self.assertEqual(hash(seg), hash(("foo", 43)))
+        seg.target = (1, 2, 3)
+        self.assertEqual(hash(seg), hash(((1, 2, 3), 43)))
+        self.assertNotEqual(hash(seg), hash(("foo", 43)))
+
+    def test_hashability(self):
+        """Tests raising of exceptions when trying to hash with non-hashable targets."""
+        seg = TargetSegment()
+        try:
+            hash(seg)
+            seg.target = "Hawai'i"
+            hash(seg)
+            seg.target = 12
+            hash(seg)
+            seg.target = (1, 2, "c")
+            hash(seg)
+        except TypeError:
+            self.fail(
+                "hash() of TargetSegment raises TypeError even though target is hashable."
+            )
+        with self.assertRaises(TypeError):
+            seg.target = [1, 2, 3]
+            hash(seg)
+        with self.assertRaises(TypeError):
+            seg.target = {1, 2, 3}
+            hash(seg)
+        with self.assertRaises(RecursionError):
+            seg.target = seg
+            hash(seg)
+
+    def test_equality(self):
+        """Tests equality comparisons of two TargetSegment objects."""
+        seg1 = TargetSegment("one", 1)
+        seg2 = TargetSegment("two", 2)
+        self.assertFalse(seg1 == seg2)
+        seg2.target = "one"
+        self.assertFalse(seg1 == seg2)
+        seg2.score = 1
+        self.assertTrue(seg1 == seg2)
+
+    def test_equality_to_tuple(self):
+        """Tests equality comparison of TargetSegment and tuples/lists."""
+        seg = TargetSegment("one", 1)
+        self.assertTrue(seg == ("one", 1))
+        self.assertTrue(seg == ["one", 1])
+        self.assertFalse(seg == ("one", 2))
+        self.assertFalse(seg == ["one", 2])
+        self.assertFalse(seg == ("one", 1, 2))
+        self.assertFalse(seg == ["one", 1, 2])
+        self.assertFalse(seg == (1))
+        self.assertFalse(seg == ["one"])
+
+
+class TestTargetSchema(unittest.TestCase):
+    """Unit tests for the `transcriptionist.schemata.TargetSchema` class."""
 
     def test_schema_creation(self):
         """Tests initialisation of schemata with various dimensions."""
-        s = Schema()
+        s = TargetSchema()
         self.assertEqual(s.length, 1)
         self.assertEqual(len(s), 1)
         self.assertEqual(s.n_forms, 1)
         self.assertEqual(s.n_alternants, 0)
-        s = Schema(10, 2)
+        s = TargetSchema(10, 2)
         self.assertEqual(s.length, 10)
         self.assertEqual(len(s), 10)
         self.assertEqual(s.n_forms, 2)
@@ -21,7 +149,7 @@ class TestSchema(unittest.TestCase):
 
     def test_set_and_read_base(self):
         """Tests assigning and reading the base form of a Schema."""
-        s = Schema(5)
+        s = TargetSchema(5)
         base_seq = ["a", "b", "c", "d", "e"]
         # From list
         s.base = base_seq
@@ -41,7 +169,7 @@ class TestSchema(unittest.TestCase):
 
     def test_set_and_read_alternants(self):
         """Tests assigning and reading the alternants of a Schema."""
-        s = Schema(2, 5)
+        s = TargetSchema(2, 5)
         self.assertEqual(s.alternants, [[None, None] for _ in range(0, 4)])
         s.set_alternant(0, ["a", "b"])
         self.assertEqual(s.alternants[0], ["a", "b"])
--- transcriptionist/schemata.py
+++ transcriptionist/schemata.py
@@ -5,17 +5,147 @@ This submodule provides a mechanism for defining scored schemas of sequences
 is derived, each which has a score based on the compound of the schema's components it
 was derived from.
 """
-from typing import Sequence
+from __future__ import annotations
+from typing import Any, Sequence, Union
 from .matrices import Matrix
 
 
-class Schema:
+class TargetSegment:
+    """Implementation of Target Segments.
+
+    Target Segments are representations of individual segments of a Target Schema,
+    consisting of the target segment itself and an associated score compounded into
+    derived target sequences.
+    """
+
+    target: Any
+    __score: Union[int, float]
+
+    def __init__(self, target: Any = None, score: Union[int, float] = 0):
+        """Initialises a new Target Segment.
+
+        Args:
+            target: The target segment.
+            score: The numeric score associated with the target segment.
+
+        Note:
+            `TargetSegment` objects are hashable as long as the type of its `target`
+            property is hashable.
+
+        Returns:
+            A new Target Segment instance.
+        """
+        self.target = target
+        self.score = score
+
+    @classmethod
+    def from_pair(
+        cls, pair: Union[list[Any, Union[int, float]], tuple[Any, Union[int, float]]]
+    ) -> TargetSegment:
+        """Instantiates a new Target Segment from a 2-tuple or list.
+
+        Args:
+            pair: A tuple or list of length two, where the first member is the target and
+                the second member is a numeric score value.
+
+        Returns:
+            A new Target Segment instance.
+        """
+        target, score = list(pair)
+        return cls(target, score)
+
+    @property
+    def score(self) -> Union[int, float]:
+        """The score awarded for sequences including this Target Segment.
+
+        Returns:
+            The numeric score of the Target Segment.
+        """
+        return self.__score
+
+    @score.setter
+    def score(self, score: Union[int, float]) -> None:
+        """Sets the score of the Target Segment.
+
+        Args:
+            score: A numeric value for the score of the Target Segment.
+        """
+        if not isinstance(score, (int, float)):
+            raise TypeError(
+                (
+                    f"Target Segment score must be numeric (int or float), "
+                    f"{type(score)} given."
+                )
+            )
+        self.__score = score
+
+    def __repr__(self) -> str:
+        """Returns a parseable representation of a Target Segment.
+
+        Returns:
+            The parseable representation of the Segment.
+        """
+        return f"{type(self).__name__}({repr(self.target)}, {repr(self.score)})"
+
+    def __str__(self) -> str:
+        """Returns a human-readable representation of the Target Segment.
+
+        Returns:
+            The string representation of the Segment.
+        """
+        return f"({str(self.target)}, {str(self.score)})"
+
+    def __hash__(self) -> int:
+        """Returns a hash based on the target and it's score.
+
+        Returns:
+            The hash of combining the target and score in a tuple.
+
+        Raises:
+            TypeError: If the `target` property of the TargetSegment object is itself not
+                hashable.
+            RecursionError: If the `target` is the TargetSegment object itself.
+        """
+        try:
+            h = hash((self.target, self.score))
+            return h
+        except TypeError:
+            return NotImplemented  # TargetSegments are hashable iff self.target is
+
+    def __eq__(self, other) -> bool:
+        """Compares itself to another Target Segment or 2-tuple.
+
+        Args:
+            other: Another Segment, or a tuple or list of length 2.
+
+        Returns:
+            True if the other item matches the target and score of the Segment,
+            False otherwise.
+        """
+        if isinstance(other, type(self)):
+            return self.target == other.target and self.score == other.score
+        if isinstance(other, (tuple, list)) and len(other) == 2:
+            return tuple(other) == (self.target, self.score)
+        return NotImplemented
+
+
+class TargetForm:
+    """Implementation of Target Forms.
+
+    A Target Form is an ordered sequence of segment-score pairs representing the base or
+    an alternant in a Target Schema.
+    """
+
+    pass
+
+
+class TargetSchema:
     """Implementation of Target Schemas."""
 
     __matrix: Matrix
 
     def __init__(self, length: int = 1, n_forms: int = 1):
-        """Initialises a new Schema.
+        """Initialises a new Target Schema.
 
         Raises:
             ValueError: Raised if a length or number of forms below 1 is specified.
@@ -23,6 +153,20 @@ class Schema:
         if length < 1 or n_forms < 1:
             raise ValueError("Length and forms must be integers greater than 0.")
         self.__matrix = Matrix(n_forms, length)
+
+    @classmethod
+    def from_matrix(cls, matrix: Matrix) -> TargetSchema:
+        """Instantiates a new TargetSchema from a matrix.
+
+        Args:
+            matrix: A Matrix representing a TargetSchema.
+
+        Returns:
+            A new TargetSchema instance.
+        """
+        ts = cls(matrix.n_cols, matrix.n_cols)
+        ts.__matrix = matrix.copy()
+        return ts
 
     @property
     def length(self):
